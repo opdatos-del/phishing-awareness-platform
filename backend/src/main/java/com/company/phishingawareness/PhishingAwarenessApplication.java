db6183b1b2505e9a6639a111a3ce12a291dabf28
opdatos-del <opdatos@jovycandy.com>
2026-09-04 08:13:33 -0600

    backend: Spring Boot skeleton with pom.xml, Dockerfile, application.yml

---
+++ backend/src/main/java/com/company/phishingawareness/PhishingAwarenessApplication.java
@@ -0,0 +1,12 @@
+package com.company.phishingawareness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingAwarenessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhishingAwarenessApplication.class, args);
+    }
+}
